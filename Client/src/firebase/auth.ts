9ee83c8d0dee753987b6afa9be6c20bd6875b28c
Castro19 <ccastroo@calpoly.edu>
2024-04-29 07:43:30 -0700

    MongoDM Implementation
    MongoDB is now implemented with a basic user schema that will store the firebase id, user first name, and last name.
    
    Next steps include preserving chat log history.

--- Client/src/firebase/auth.ts
+++ Client/src/firebase/auth.ts
@@ -19,7 +19,38 @@ export const doCreateUserWithEmailAndPassword = async (
   lastName: string
 ): Promise<UserCredential> => {
   console.log("SignUP: ", firstName, lastName);
-  return createUserWithEmailAndPassword(auth, email, password);
+  const userCredential: UserCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  const firebaseUserId = userCredential.user.uid; // Get the Firebase user ID
+  // Now, send a request to your backend to store additional user information
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firebaseUserId,
+        firstName,
+        lastName,
+      }),
+    };
+    console.log("Body Object: ", options);
+    const response = await fetch("http://localhost:4000/users/signup", options);
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Backend registration failed", errorData);
+      throw new Error("Backend registration failed: " + errorData.message);
+    }
+    const responseData = await response.json();
+    console.log("Backend Registration worked!: ", responseData);
+  } catch (error) {
+    console.error("Backend registration failed", error);
+  }
+  return userCredential;
 };
 
 export const doSignInWithEmailAndPassword = async (
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import OpenAI from "openai";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+// Routes:
+import llms from "./routes/llm.js";
+import users from "./routes/user.js";
 import { fileURLToPath } from "url";
 import path, { dirname, join } from "path";
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import chooseModel from "./utils/chooseModel.js"; // Make sure the file has `.js` extension or is a directory with an `index.js` file
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -16,92 +16,20 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 4000;
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
-});
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.text());
 
+// Routes
+app.use("/llms", llms);
+app.use("/users", users);
+
 // Serve static files from the React app
 // Calculate __dirname equivalent in ES Module
-
 // Now you can use __dirname as before
 const clientBuildPath = join(__dirname, "../Client/dist/");
 app.use(express.static(clientBuildPath));
-
-// Route to handle POST requests
-app.post("/respond", async (req, res) => {
-  try {
-    const { message, modelType } = req.body;
-
-    const modelDesc = chooseModel(modelType);
-    const contentStr =
-      "You are a helpful assistant. When you provide explanations or answers, format them using Markdown syntax. For example, use ** for bold text where emphasis is needed. " +
-      modelDesc;
-    console.log("MODEL TYPE: ", modelType);
-    console.log("Server Side Model Description: ", modelDesc);
-    // model: "gpt-3.5-turbo-0125",
-    // model: "gpt-4-0613",
-    const chatCompletion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-0125",
-      messages: [
-        {
-          role: "system",
-          content: contentStr,
-        },
-        { role: "user", content: message },
-      ],
-      stream: true,
-    });
-    const reader = chatCompletion.toReadableStream().getReader();
-    // Send the ChatGPT response back to the client
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
-        break;
-      }
-      res.write(value);
-    }
-    res.end();
-  } catch (error) {
-    console.error("Error calling OpenAI:", error);
-    res.status(500).json({ error: "Failed to generate response from OpenAI" });
-  }
-});
-
-app.post("/title", async (req, res) => {
-  try {
-    const { message, modelType } = req.body;
-
-    const modelDesc = chooseModel(modelType);
-    const contentStr =
-      "Based on the user's message and the model description, please return a 10-30 character title response that best suits the user's message. Important The response should not be larger than 30 chars and should be a title! Model Description:" +
-      modelDesc;
-    console.log("Bot Instructions: ", contentStr);
-    // model: "gpt-3.5-turbo-0125",
-    // model: "gpt-4-0613",
-    const chatCompletion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-0125",
-      messages: [
-        {
-          role: "system",
-          content: contentStr,
-        },
-        { role: "user", content: message },
-      ],
-    });
-
-    // Send the ChatGPT response back to the client
-    const title = chatCompletion.choices[0].message.content;
-    console.log("TITLE: ", title);
-    res.json({ title: title });
-  } catch (error) {
-    console.error("Error calling OpenAI:", error);
-    res.status(500).json({ error: "Failed to generate response from OpenAI" });
-  }
-});
 
 // Catch-all handler for any other client-side routes
 app.get("*", (req, res) => {
@@ -115,5 +43,5 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
