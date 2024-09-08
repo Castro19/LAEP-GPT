caca439366f1c98c5ef09110f603f85ce9302328
nathanlim1 <nlim10@calpoly.edu>
2024-09-08 16:34:10 -0700

    Implementation of surface level access control--only users with userType "teacher" are able to create additional assistants. Modified authSlice.ts to properly update the userType on auth changes

--- Client/src/components/customGPT/GPTContainer/GPTContainer.tsx
+++ Client/src/components/customGPT/GPTContainer/GPTContainer.tsx
@@ -2,20 +2,28 @@ import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import GPTList from "../GPTList/GPTList";
 import styles from "./GPTContainer.module.css";
+import { useAppSelector } from "@/redux"; 
 
 const GPTContainer = () => {
   const navigate = useNavigate();
+  const userType = useAppSelector((state) => state.auth.userType);
+
   const onClick = () => {
     console.log("Button was clicked to create a custom GPT");
     navigate(`editor`);
   };
+
   return (
     <div>
       <h1 className={styles.pageTitle}>Select a CalPoly Assistant</h1>
       <GPTList />
-      <Button className="w-full" onClick={onClick}>
-        Create
-      </Button>{" "}
+      
+      {/* only render the button if userType is not student */}
+      {userType !== "student" && (
+        <Button className="w-full" onClick={onClick}>
+          Create A New Assistant
+        </Button>
+      )}
     </div>
   );
 };
