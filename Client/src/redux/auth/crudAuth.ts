fc37006103befaab6e94f1e7de8538aba50fdb58
Castro19 <ccastroo@calpoly.edu>
2024-10-25 15:57:39 -0700

    Added new Protected Route for new Users

--- Client/src/redux/auth/crudAuth.ts
+++ Client/src/redux/auth/crudAuth.ts
@@ -2,15 +2,14 @@
 export async function loginUser(token: string): Promise<any> {
   // Send the token to the server
   try {
-    const options = {
+    const response = await fetch("http://localhost:4000/auth/login", {
       method: "POST",
       credentials: "include", // Include credentials to allow cookies to be set
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ token }),
-    };
-    const response = await fetch("http://localhost:4000/auth/login", options);
+    });
     const responseData = await response.json();
     return responseData;
   } catch (error) {
