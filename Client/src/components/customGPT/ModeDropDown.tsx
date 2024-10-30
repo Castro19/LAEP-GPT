550f24d1e8ff5f6dcaa94ae236ca112a48cbdb13
Castro19 <ccastroo@calpoly.edu>
2024-10-30 14:38:29 -0700

    ModeDropDown UI Improved

--- Client/src/components/customGPT/ModeDropDown.tsx
+++ Client/src/components/customGPT/ModeDropDown.tsx
@@ -1,16 +1,26 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { layoutActions, useAppDispatch, useAppSelector } from "@/redux";
 // UI
+
+import { GptType, MyUserInfo } from "@/types";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { GptType } from "@/types";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import React from "react";
+import { cn } from "@/lib/utils";
 
 type ModeDropDownProps = {
   // eslint-disable-next-line no-unused-vars
   onSelect: (model: GptType) => void;
 };
 
-const ModeDropDown = ({ onSelect }: ModeDropDownProps) => {
+export default function ModeDropDown({ onSelect }: ModeDropDownProps) {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const userId = useAppSelector((state) => state.auth.userId);
@@ -18,51 +28,35 @@ const ModeDropDown = ({ onSelect }: ModeDropDownProps) => {
   const isDropdownVisible = useAppSelector(
     (state) => state.layout.isDropdownVisible
   );
-
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const [maxHeight, setMaxHeight] = useState("0");
+  const { userData } = useAppSelector((state) => state.user);
+  const [matchingAssistantLocked, setMatchingAssistantLocked] = useState(true);
 
   useEffect(() => {
-    if (isDropdownVisible && dropdownRef.current) {
-      const fullHeight = dropdownRef.current.scrollHeight + "px";
-      setMaxHeight(fullHeight);
-    } else {
-      setMaxHeight("0");
-    }
-  }, [isDropdownVisible]);
+    const userMatchingAssistantLocked = (userData: MyUserInfo) => {
+      const { availability, interests } = userData;
 
-  // Handle clicks outside the dropdown
-  useEffect(() => {
-    if (!isDropdownVisible) return;
-
-    // Close the dropdown if the click is outside the modeDropDown component
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        dispatch(layoutActions.toggleDropdown(false));
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    // Cleanup function to remove the event listener when the component unmounts
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      const hasNoAvailability = Object.values(availability).every(
+        (timeSlots) => timeSlots.length === 0
+      );
+      return interests.length === 0 || hasNoAvailability;
     };
-  }, [dispatch, isDropdownVisible]);
-
+    const matchingAssistantLocked = userMatchingAssistantLocked(userData);
+    setMatchingAssistantLocked(matchingAssistantLocked);
+  }, [userData]);
   // Transform gptList to prioritize 'name' over 'title'
   const transformedGptList = gptList.map((option) => {
     // Prioritize displaying 'name', fallback to 'title' if 'name' is not available
     const title = option.title || "Unnamed Assistant";
     const description = option.desc || "No description available";
-
+    if (matchingAssistantLocked && option.title === "matchingAssistant") {
+      console.log("matchingAssistantLocked", matchingAssistantLocked);
+    }
     return {
       id: option.id,
       title,
       desc: description,
       urlPhoto: option.urlPhoto || "", // Default to an empty string if no photo is provided
+      locked: title === "Matching Assistant" && matchingAssistantLocked,
     };
   });
 
@@ -73,48 +67,67 @@ const ModeDropDown = ({ onSelect }: ModeDropDownProps) => {
   };
 
   return (
-    <div
-      ref={dropdownRef}
-      className="fixed mt-2 w-48 bg-white border border-gray-200 shadow-lg rounded-md overflow-hidden z-10 transition-max-height duration-500 ease-in-out"
-      style={{ maxHeight }}
-    >
-      {isDropdownVisible &&
-        transformedGptList.map((option) => (
-          <div
-            key={option.id}
-            className="flex items-center pr-4 py-2 hover:bg-gray-300 hover:text-white"
-          >
-            <div className="pointer-events-none">
-              <Avatar className="w-10 h-10 rounded-full overflow-hidden transition-transform hover:scale-110">
-                <AvatarImage
-                  src={option.urlPhoto || "/imgs/test.png"}
-                  alt="Assistant Photo"
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>{currentModel.title}</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] bg-white dark:bg-gray-900">
+              <li className="row-span-3 mb-3 mr-3 border-b pb-3 lg:border-b-0 lg:border-r border-gray-200 dark:border-gray-700">
+                <div className="text-lg font-medium">{currentModel.title}</div>
+                <p className="text-sm leading-tight text-muted-foreground">
+                  {currentModel.desc}
+                </p>
+              </li>
+              {transformedGptList.map((option) => (
+                <ListItem
+                  key={option.id}
+                  gpt={option}
+                  disabled={option.locked}
+                  onClick={() => onSelect(option)}
+                  className={option.locked ? "cursor-not-allowed" : ""}
                 />
-              </Avatar>
-            </div>
-            <button
-              onClick={() => onSelect(option)}
-              className="flex justify-between items-center w-full ml-2 text-left text-sm text-gray-700"
-            >
-              {option.title} {/* Ensure the title or name is displayed */}
-              <span
-                className={`${
-                  currentModel.id === option.id
-                    ? "bg-blue-500"
-                    : "bg-transparent border border-gray-400"
-                } inline-block ml-2 rounded-full w-4 h-4`}
-              ></span>
-            </button>
-          </div>
-        ))}
-      <button
-        onClick={onViewGPTs}
-        className="flex justify-between items-center w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-900 hover:text-white"
-      >
-        View Assistant Details
-      </button>
-    </div>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
-};
+}
 
-export default ModeDropDown;
+const ListItem = React.forwardRef<
+  React.ElementRef<"button">,
+  React.ComponentPropsWithoutRef<"button"> & { gpt: GptType }
+>(({ className, gpt, disabled, children, ...props }, ref) => {
+  const { id, title, urlPhoto } = gpt;
+  return (
+    <button
+      ref={ref}
+      disabled={disabled}
+      className={cn(
+        "block w-full select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-slate-700 hover:text-white focus:bg-slate-700 focus:text-white",
+        className
+      )}
+      {...props}
+    >
+      <div className="flex items-center space-x-4">
+        <Avatar className="w-10 h-10 rounded-full overflow-hidden transition-transform hover:scale-110">
+          <AvatarImage
+            src={urlPhoto || "/imgs/test.png"}
+            alt="Assistant Photo"
+          />
+        </Avatar>
+        <div>
+          <div className="text-sm font-medium leading-none">{title}</div>
+          {children && (
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          )}
+        </div>
+      </div>
+    </button>
+  );
+});
+ListItem.displayName = "ListItem";
