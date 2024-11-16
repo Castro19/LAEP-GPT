5677de05d22b21e4aad7b4e6dd8de30947dcd538
Castro19 <ccastroo@calpoly.edu>
2024-11-16 12:11:04 -0800

    Drag&Drop Classes

--- Client/src/types/flowChart/flowChartTypes.ts
+++ Client/src/types/flowChart/flowChartTypes.ts
@@ -24,11 +24,6 @@ export interface CatalogOption {
   major: string;
   concentration: string;
 }
-export interface PostFlowchartInDB {
-  flowchartData: FlowchartData;
-  name: string;
-  primaryOption?: boolean;
-}
 
 export interface FlowchartData {
   id: string;
@@ -47,6 +42,11 @@ export interface FlowchartData {
 }
 
 // api response types
+export interface PostFlowchartInDB {
+  flowchartData: FlowchartData;
+  name: string;
+  primaryOption?: boolean;
+}
 export interface CreateFlowchartResponse {
   flowchartId: string;
   name: string;
