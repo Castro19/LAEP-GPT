c3767d68b2afb6da7d3abb19b92f19ac5a1f3630
Castro19 <ccastroo@calpoly.edu>
2024-11-19 13:14:43 -0800

    Flowchart Primary Updated
    Flowchart Primary Styles

--- Client/src/components/flowchart/flowchartLog/FlowchartLog.tsx
+++ Client/src/components/flowchart/flowchartLog/FlowchartLog.tsx
@@ -51,11 +51,20 @@ const FlowchartLog = ({ flowchart, onSelectFlowchart }: FlowchartLogProps) => {
       <SidebarMenuButton
         asChild
         onClick={() => handleSelectFlowchart(flowchart.flowchartId)}
-        className={`flex-grow h-full text-left border border-gray-600 bg-slate-800 rounded-md transition duration-200 ease-in-out transform hover:bg-slate-700 hover:scale-105 ${
+        className={`flex-grow h-full text-left border ${
+          primaryOption
+            ? "border-yellow-600 border-2" // Primary flowchart style
+            : "border-gray-600"
+        } bg-slate-800 rounded-md transition duration-200 ease-in-out transform hover:bg-slate-700 hover:scale-105 ${
           flowchartId === flowchart.flowchartId ? "bg-slate-600" : ""
         }`}
       >
-        <span className="font-semibold truncate text-white">{name}</span>
+        <span
+          className={`font-semibold truncate ${primaryOption ? "text-yellow-500" : "text-white"}`}
+        >
+          {primaryOption && "★ "}
+          {name}
+        </span>
       </SidebarMenuButton>
 
       {/* Flowchart options */}
