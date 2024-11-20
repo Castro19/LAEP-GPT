98b0dade725451d54fd52095db54f6e1ac19b9a7
Castro19 <ccastroo@calpoly.edu>
2024-11-20 13:43:56 -0800

    ChatLog Sidebar Footer

--- Client/src/components/userProfile/UserMenu.tsx
+++ Client/src/components/userProfile/UserMenu.tsx
@@ -1,45 +1,24 @@
-import { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // Icon
-import { FaSignOutAlt } from "react-icons/fa";
+
 // Component
 import UserAvatar from "./UserAvatar";
 import { Button } from "../ui/button";
 // Redux
-import { useAppDispatch, useAppSelector } from "@/redux";
-import { signOutUser } from "@/redux/auth/authSlice";
+import { useAppSelector } from "@/redux";
 
 const UserMenu = () => {
-  const navigate = useNavigate();
-  const [isHovered, setIsHovered] = useState(false);
-  const dispatch = useAppDispatch();
   const { userLoggedIn } = useAppSelector((state) => state.auth);
   const { userData } = useAppSelector((state) => state.user);
-  const handleSignOut = () => {
-    dispatch(signOutUser({ navigate })); // Trigger the thunk to sign out the user
-  };
 
   return (
-    <div className="relative">
+    <>
       {userLoggedIn ? (
         <div className="flex justify-start items-center space-x-4">
           <UserAvatar />
           <h4 className="ml-4 text-gray-800 dark:text-gray-300 font-medium text-wrap overflow-y-auto overflow-x-hidden">
             {userData?.name}
           </h4>
-          <button
-            className="text-lg hover:text-red-500"
-            onClick={handleSignOut}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            <FaSignOutAlt />
-            {isHovered && (
-              <span className="absolute right-0  translate-y-1 bg-gray-800 text-white px-2 py-1 rounded-md whitespace-nowrap">
-                Sign Out
-              </span>
-            )}
-          </button>
         </div>
       ) : (
         <div className="flex justify-center items-center fixed inset-x-0 bottom-1 shadow-2xl bg-gray-50 dark:bg-gray-800 py-3">
@@ -53,7 +32,7 @@ const UserMenu = () => {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
