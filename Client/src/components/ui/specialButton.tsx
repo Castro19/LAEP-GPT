a0cfff36029574f8e705df0b0bd839084a33a311
Castro19 <ccastroo@calpoly.edu>
2024-11-20 11:03:39 -0800

    SpecialButton Styles

--- Client/src/components/ui/specialButton.tsx
+++ Client/src/components/ui/specialButton.tsx
@@ -11,7 +11,7 @@ const SpecialButton: React.FC<SpecialButtonProps> = ({ onClick, text }) => {
     <div className="flex flex-col justify-center items-center">
       <button
         onClick={onClick}
-        className="w-full p-2 border rounded-lg bg-gray-100 dark:bg-indigo-800 dark:text-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 dark:hover:bg-indigo-700"
+        className="w-3/4 p-2 border rounded-lg bg-gray-100 dark:bg-indigo-800 dark:text-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 dark:hover:bg-indigo-700"
       >
         <div className="flex items-center justify-center gap-2">
           {text}
