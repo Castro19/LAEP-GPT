9ae31084b86da1f0e86163e490c66bc6ac01443e
Castro19 <ccastroo@calpoly.edu>
2025-01-12 14:05:54 -0800

    Suggested New UserData Type

--- shared/src/types/user/index.ts
+++ shared/src/types/user/index.ts
@@ -25,3 +25,29 @@ export type UserData = {
 
 export type UserDocument = UserData & { _id: string };
 export type UpdateUserData = Partial<UserData>;
+
+export type NewUserData = {
+  userId: string;
+  name: string;
+  userType: "student" | "admin";
+  email: string;
+  emailVerified: boolean;
+
+  availability: Availability;
+  bio: string;
+  year: "freshman" | "sophomore" | "junior" | "senior" | "graduate";
+  demographic: {
+    gender: string;
+    ethnicity: string;
+  };
+  interestAreas: string[];
+  preferredActivities: string[];
+
+  flowchartInformation: {
+    flowchartId: string; // id of primary flowchart
+    startingYear: string;
+    catalog: string;
+    major: string;
+    concentration: string;
+  };
+};
