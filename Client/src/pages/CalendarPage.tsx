fa04929b84756a0bf3a91a7a87a69ff0ff4d7982
Castro19 <ccastroo@calpoly.edu>
2025-02-06 07:33:17 -0800

    Calendar Layout setup pt.2

--- Client/src/pages/CalendarPage.tsx
+++ Client/src/pages/CalendarPage.tsx
@@ -1,8 +1,8 @@
-import SectionPageLayout from "@/components/layout/SectionPage/SectionPageLayout";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import AdminViewOnly from "@/components/security/AdminViewOnly";
 import OuterSidebar from "@/components/layout/OuterIconSidebar";
 import CalendarContainer from "@/components/calendar/CalendarContainer";
+import CalendarPageLayout from "@/components/layout/CalendarPage/CalendarPageLayout";
 
 const CalendarPage = () => {
   return (
@@ -10,14 +10,14 @@ const CalendarPage = () => {
       <div className="flex">
         <OuterSidebar />
         <SidebarProvider className="dark:bg-slate-900">
-          <SectionPageLayout>
+          <CalendarPageLayout>
             <div className="grid grid-cols-1 md:grid-cols-4 grid-rows-1 gap-4">
               <div className="col-span-1"></div>
               <div className="col-span-3">
                 <CalendarContainer />
               </div>
             </div>
-          </SectionPageLayout>
+          </CalendarPageLayout>
         </SidebarProvider>
       </div>
     </AdminViewOnly>
