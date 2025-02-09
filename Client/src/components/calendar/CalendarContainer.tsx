4c1bd54f583be01b70ac3f9b57f1f0d3171b67fb
Castro19 <ccastroo@calpoly.edu>
2025-02-09 14:12:22 -0800

    Weekly Calendar Slots

--- Client/src/components/calendar/CalendarContainer.tsx
+++ Client/src/components/calendar/CalendarContainer.tsx
@@ -1,10 +1,16 @@
 import WeeklyCalendar from "./WeeklyCalendar";
-
+import { useAppSelector } from "@/redux";
 const CalendarContainer = () => {
+  const { selectedSections } = useAppSelector(
+    (state) => state.sectionSelection
+  );
+  if (selectedSections.length === 0 || !Array.isArray(selectedSections)) {
+    return <div>No sections chosen</div>;
+  }
   return (
     <div className="flex flex-col gap-4 w-full min-h-screen overflow-hidden no-scroll pb-12">
       <div className="overflow-auto flex-1 no-scroll">
-        <WeeklyCalendar />
+        <WeeklyCalendar sections={selectedSections} />
         {/* Could be footer */}
         {/* <PaginationFooter /> */}
       </div>
