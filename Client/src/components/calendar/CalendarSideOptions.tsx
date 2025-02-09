eab4062688f893e2ebbefe7a10a2c9590d398ee6
Castro19 <ccastroo@calpoly.edu>
2025-02-09 13:08:19 -0800

    Handle Build Schedule Button

--- Client/src/components/calendar/CalendarSideOptions.tsx
+++ Client/src/components/calendar/CalendarSideOptions.tsx
@@ -1,19 +1,36 @@
+import AnimateWrapper from "../section/AnimateWrapper";
+import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-const CalendarSideOptions = ({ children }: { children: React.ReactNode }) => {
+const CalendarSideOptions = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) => {
   return (
-    <div className="flex flex-col h-full">
-      <Card className="flex flex-col border-0 shadow-lg no-scroll max-h-[83%]">
-        <div className="overflow-auto flex-1 no-scroll">
-          <ScrollArea className="h-full min-w-full mb-4">
-            <div className="p-4"></div>
-
-            <div className="px-6 space-y-4 pb-4">{children}</div>
-          </ScrollArea>
+    <AnimateWrapper>
+      <div className="flex flex-col h-full">
+        <Card className="flex flex-col border-0 shadow-lg no-scroll max-h-[83%]">
+          <div className="overflow-auto flex-1 no-scroll">
+            <ScrollArea className="h-full min-w-full mb-4">
+              <div className="px-6 space-y-4 pb-4">{children}</div>
+            </ScrollArea>
+          </div>
+        </Card>
+        {/* Divider */}
+        <div className="border-t border-gray-200 p-4" />
+        {/* Sticky footer with Reset and Apply */}
+        <div className="sticky bottom-0 mx-4 bg-background/95 backdrop-blur flex gap-2 shadow-lg">
+          {/* Apply Filters button */}
+          <Button type="submit" className="w-full shadow-lg" onClick={onClick}>
+            Build Schedule
+          </Button>
         </div>
-      </Card>
-    </div>
+      </div>
+    </AnimateWrapper>
   );
 };
 
