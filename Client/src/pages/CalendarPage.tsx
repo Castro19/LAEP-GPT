14f728c31869ac228b8cf21847d65e55429e38df
Castro19 <ccastroo@calpoly.edu>
2025-02-09 13:08:19 -0800

    Handle Build Schedule Button

--- Client/src/pages/CalendarPage.tsx
+++ Client/src/pages/CalendarPage.tsx
@@ -14,12 +14,16 @@ const CalendarPage = () => {
     dispatch(sectionSelectionActions.fetchSelectedSectionsAsync());
   }, [dispatch]);
 
+  const handleBuildSchedule = () => {
+    console.log("Build Schedule");
+  };
+
   return (
     <AdminViewOnly>
       <CalendarPageLayout>
         <div className="grid grid-cols-1 md:grid-cols-4 grid-rows-1 gap-4">
           <div className="col-span-1">
-            <CalendarSideOptions>
+            <CalendarSideOptions onClick={handleBuildSchedule}>
               <SelectedSectionContainer />
             </CalendarSideOptions>
             {/* <ScheduleBuilderQueryForm /> */}
