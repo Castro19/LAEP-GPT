eb2dc7bd7b6f4bed374135b8224fbb50cd2122ad
Jake Huey <125854542+jhueyy@users.noreply.github.com>
2025-02-10 17:30:34 -0800

    fixing merge

--- Client/src/pages/ProfilePage.tsx
+++ Client/src/pages/ProfilePage.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector, flowchartActions } from "@/redux";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector, flowchartActions } from "@/redux";
 import { useUserData } from "@/hooks/useUserData";
 import { toast } from "@/components/ui/use-toast";
 import { environment } from "@/helpers/getEnvironmentVars";
