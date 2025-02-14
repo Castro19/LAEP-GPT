c4a894227b95b34f5e47476697d0ce27d69400a1
Castro19 <ccastroo@calpoly.edu>
2025-02-14 15:58:35 -0800

    Mobile Layout for ProfilePage

--- Client/src/components/layout/ProfilePage/ProfilePageLayout.tsx
+++ Client/src/components/layout/ProfilePage/ProfilePageLayout.tsx
@@ -4,18 +4,22 @@ import ProfilePageHeader from "./ProfilePageHeader";
 import DragDropContextWrapper from "../DragDropContxtWrapper";
 import OuterSidebar from "../OuterIconSidebar";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import useIsMobile from "@/hooks/use-mobile";
+import MobileHeader from "../MobileHeader";
 
 type ProfilePageLayoutProps = {
   children: ReactNode;
 };
 const ProfilePageLayout = ({ children }: ProfilePageLayoutProps) => {
+  const isMobile = useIsMobile();
+
   return (
     <div className="min-h-screen">
       <div className="flex">
-        <OuterSidebar />
+        {isMobile ? null : <OuterSidebar />}
         <DragDropContextWrapper>
           <div className="bg-slate-800 text-white min-h-screen flex flex-col no-scroll w-full oveflow-hidden">
-            <ProfilePageHeader />
+            {isMobile ? <MobileHeader /> : <ProfilePageHeader />}
             <ScrollArea className="overflow-y-auto">
               <div className="flex-1 h-[95vh]">{children}</div>
             </ScrollArea>
