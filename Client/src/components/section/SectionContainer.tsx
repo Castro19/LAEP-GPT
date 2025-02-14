181f2696920c285f8bf97697ec467a6a252f56a6
Castro19 <ccastroo@calpoly.edu>
2025-02-14 15:43:30 -0800

    Footers Mobile styling

--- Client/src/components/section/SectionContainer.tsx
+++ Client/src/components/section/SectionContainer.tsx
@@ -6,19 +6,24 @@ import { PaginationFooter } from "./PaginationFooter";
 import NoSectionsFound from "./NoSectionsFound";
 import InitialSectionState from "./InitialSectionState";
 import SectionLoading from "./SectionLoading";
+import useIsMobile from "@/hooks/use-mobile";
+
 const SectionContainer = () => {
+  const isMobile = useIsMobile();
   const { sections, isInitialState, loading } = useAppSelector(
     (state) => state.section
   );
   const courses = transformSectionsToCatalog(sections);
 
   return (
-    <div className="flex flex-col gap-4 w-full min-h-screen overflow-hidden no-scroll pb-12">
+    <div className="flex flex-col gap-4 w-full min-h-screen overflow-hidden no-scroll">
       {loading ? (
         <SectionLoading />
       ) : (
         <div className="overflow-auto flex-1 no-scroll">
-          <ScrollArea className="h-full min-w-full pb-12">
+          <ScrollArea
+            className={`${isMobile ? "h-[75%]" : "h-[85%]"} min-w-full pb-12`}
+          >
             {courses.length > 0 ? (
               <>
                 <CourseCatalog courses={courses} />
