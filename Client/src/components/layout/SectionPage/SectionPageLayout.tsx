beabaad2e1ad657150516aaa6d1f931bcc7d769e
Castro19 <ccastroo@calpoly.edu>
2025-02-14 14:20:33 -0800

    Mobile Icon Header

--- Client/src/components/layout/SectionPage/SectionPageLayout.tsx
+++ Client/src/components/layout/SectionPage/SectionPageLayout.tsx
@@ -4,6 +4,8 @@ import { DragDropContext, DropResult } from "@hello-pangea/dnd";
 
 import { handleDragEnd } from "@/redux/panelLayout/panelLayoutSlice";
 import { useAppDispatch } from "@/redux";
+import MobileHeader from "../MobileHeader";
+import useMobile from "@/hooks/use-mobile";
 
 type SectionPageLayoutProps = {
   children: React.ReactNode;
@@ -11,6 +13,7 @@ type SectionPageLayoutProps = {
 
 const SectionPageLayout = ({ children }: SectionPageLayoutProps) => {
   const dispatch = useAppDispatch();
+  const isMobile = useMobile();
   // Handle drag end: update the order of panels and (for now)
   // assign a placeholder outerDirection value.
   const onDragEnd = (result: DropResult) => {
@@ -28,8 +31,17 @@ const SectionPageLayout = ({ children }: SectionPageLayoutProps) => {
     <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
       {/* <SectionPageSidebar /> */}
       <div className="bg-slate-900 text-white min-h-screen flex flex-col no-scroll w-full ml-2">
-        <SectionPageHeader />
-        <div className="flex-1">{children}</div>
+        {isMobile ? (
+          <>
+            <MobileHeader />
+            <div className="flex-1">{children}</div>
+          </>
+        ) : (
+          <>
+            <SectionPageHeader />
+            <div className="flex-1">{children}</div>
+          </>
+        )}
       </div>
     </DragDropContext>
   );
