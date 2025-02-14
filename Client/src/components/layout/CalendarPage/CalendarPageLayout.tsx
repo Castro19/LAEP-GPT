6f92bb7134742b0a8ce8f93ea5d4b30d9b22c5e8
Castro19 <ccastroo@calpoly.edu>
2025-02-14 14:43:23 -0800

    Calendar Page Mobile Layout

--- Client/src/components/layout/CalendarPage/CalendarPageLayout.tsx
+++ Client/src/components/layout/CalendarPage/CalendarPageLayout.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import CalendarPageHeader from "./CalendarPageHeader";
 import OuterSidebar from "../OuterIconSidebar";
+import useMobile from "@/hooks/use-mobile";
+import MobileHeader from "../MobileHeader";
 
 type SectionPageLayoutProps = {
   children: React.ReactNode;
 };
 
 const CalendarPageLayout = ({ children }: SectionPageLayoutProps) => {
+  const isMobile = useMobile();
   return (
     <div className="min-h-screen">
       <div className="flex">
-        <OuterSidebar />
-        <div className="bg-slate-900 text-white min-h-screen flex flex-col no-scroll w-full ml-2">
-          <CalendarPageHeader />
-          <div className="flex-1">{children}</div>
+        {isMobile ? null : <OuterSidebar />}
+        <div
+          className={`bg-slate-900 text-white min-h-screen flex flex-col no-scroll w-full ${
+            isMobile ? "ml-0" : "ml-2"
+          }`}
+        >
+          {isMobile ? <MobileHeader /> : <CalendarPageHeader />}
+          <div className={`flex-1 ${isMobile ? "mt-16" : ""}`}>{children}</div>
         </div>
       </div>
     </div>
