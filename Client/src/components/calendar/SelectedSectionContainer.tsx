348a4d11ee94ac4b81940d55961fd9bd9fd12a7f
Cristian Castro Oliva <74516049+Castro19@users.noreply.github.com>
2025-02-15 12:06:50 -0800

    Splash Page Content Improvement (#161)

--- Client/src/components/calendar/SelectedSectionContainer.tsx
+++ Client/src/components/calendar/SelectedSectionContainer.tsx
@@ -6,9 +6,6 @@ import { FaBook, FaCalendar } from "react-icons/fa";
 const SelectedSectionContainer = () => {
   return (
     <div className="flex flex-col h-full">
-      <h1 className="text-2xl font-bold text-center mb-4">
-        Build Your Schedule
-      </h1>
       <div className="flex flex-col gap-4">
         <CollapsibleContentWrapper title="Selected Sections" icon={FaBook}>
           <SectionsChosen />
--- Client/src/components/splashPage/MatchingFeatures.tsx
+++ Client/src/components/splashPage/MatchingFeatures.tsx
@@ -2,21 +2,6 @@ import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
 
 const content = [
   {
-    title: "Search through Course Catalog",
-    description:
-      "Have you ever wondered what classes are available? PolyLink can help you find the right classes for you. You can search through the course catalog to find the classes you are interested in by giving a description of what you are looking for.",
-    content: (
-      <div className="h-full w-full flex items-center justify-center text-white">
-        <img
-          src="/imgs/splash/course-catalog.gif"
-          alt="linear board demo"
-          width={500}
-          height={500}
-        />
-      </div>
-    ),
-  },
-  {
     title: "Find the right Club for you",
     description:
       "Search through 400+ clubs Cal Poly has to offer. PolyLink can help you find the right club. We use your provided interests to find the clubs that are the best fit for you.",
@@ -32,20 +17,65 @@ const content = [
     ),
   },
   {
-    title: "Find a Professor",
+    title: "Find Your Spring 2025 Classes with AI",
     description:
-      "Utilizing ratings from Polyratings, PolyLink can help you find the right professor for you. Simply type the course name and PolyLink will summarize all professors teaching that course and their ratings.",
+      "Struggling to find the perfect class? PolyLink makes it easy! Just describe what you're looking for, and our smart search will instantly find the best course options for you—no more endless scrolling through the catalog!",
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <img
-          src="/imgs/splash/professor-ratings.gif"
-          width={500}
-          height={500}
+          src="/imgs/splash/course-ai-query.gif"
           alt="linear board demo"
+          width={680}
+          height={382}
         />
       </div>
     ),
   },
+  {
+    title: "Build Your Perfect Schedule!",
+    description:
+      "You’ve picked your classes—now let Schedule Builder handle the rest! Generate multiple schedule options, resolve conflicts, and customize with ease. Say goodbye to scheduling stress!",
+    content: (
+      <div className="h-full w-full flex items-center justify-center text-white">
+        <img
+          src="/imgs/splash/build-schedules.gif"
+          alt="linear board demo"
+          width={680}
+          height={382}
+        />
+      </div>
+    ),
+  },
+  {
+    title: "From Planning to AI-Powered Insights!",
+    description:
+      "You’ve selected your classes and generated schedules—now let AI summarize, analyze, and optimize! Get instant summaries, professor insights, and smart suggestions to help you make the most of Spring 2025.",
+    content: (
+      <div className="h-full w-full flex items-center justify-center text-white">
+        <img
+          src="/imgs/splash/schedule-builder-ai.gif"
+          alt="linear board demo"
+          width={680}
+          height={382}
+        />
+      </div>
+    ),
+  },
+  // {
+  //   title: "Find a Professor",
+  //   description:
+  //     "Utilizing ratings from Polyratings, PolyLink can help you find the right professor for you. Simply type the course name and PolyLink will summarize all professors teaching that course and their ratings.",
+  //   content: (
+  //     <div className="h-full w-full flex items-center justify-center text-white">
+  //       <img
+  //         src="/imgs/splash/professor-ratings.gif"
+  //         width={500}
+  //         height={500}
+  //         alt="linear board demo"
+  //       />
+  //     </div>
+  //   ),
+  // },
 ];
 
 export function StickyScrollRevealDemo() {
