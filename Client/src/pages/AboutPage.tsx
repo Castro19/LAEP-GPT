cae836b2bbe3cae696cec234f31501b169b35112
Castro19 <ccastroo@calpoly.edu>
2025-02-17 14:32:12 -0800

    About Page Creation

---
+++ Client/src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import SplashLayout from "@/components/layout/splashPage/SplashLayout";
+import AboutSection from "@/components/splashPage/about/AboutSection";
+
+const AboutPage = () => {
+  return (
+    <SplashLayout>
+      <AboutSection />
+    </SplashLayout>
+  );
+};
+
+export default AboutPage;
