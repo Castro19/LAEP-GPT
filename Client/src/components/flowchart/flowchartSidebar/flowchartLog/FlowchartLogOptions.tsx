594c522d88fb2d0274cd5262ba503dfb1a746c22
Cristian Castro Oliva <74516049+Castro19@users.noreply.github.com>
2025-04-06 11:49:07 -0700

    Flowchart refactor (#264)

--- Client/src/components/flowchart/flowchartSidebar/flowchartLog/FlowchartLogOptions.tsx
+++ Client/src/components/flowchart/flowchartSidebar/flowchartLog/FlowchartLogOptions.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { flowchartActions, useAppDispatch, useAppSelector } from "@/redux";
 import { useNavigate } from "react-router-dom";
 
