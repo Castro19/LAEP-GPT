d3e6b5a1b92ce2c8fc89a721ed3e71fd25b5e143
Castro19 <ccastroo@calpoly.edu>
2025-04-12 10:26:12 -0700

    flowchart side panel layout created

--- Client/src/components/layout/FlowchartPage/FlowchartLayout.tsx
+++ Client/src/components/layout/FlowchartPage/FlowchartLayout.tsx
@@ -1,10 +1,6 @@
 // FlowchartLayout.tsx
 
-// Redux
-import { useAppSelector } from "@/redux";
-
 // My components
-import { SidebarFlowchart, FlowchartUnitCounts } from "@/components/flowchart";
 import DragDropContextWrapper from "@/components/layout/dnd/DragDropContextWrapper";
 import FlowchartHeader from "@/components/layout/FlowchartPage/FlowchartHeader";
 
@@ -12,11 +8,9 @@ import FlowchartHeader from "@/components/layout/FlowchartPage/FlowchartHeader";
 import useIsNarrowScreen from "@/hooks/useIsNarrowScreen";
 
 const FlowchartLayout = ({ children }: { children: React.ReactNode }) => {
-  const { flowchartData } = useAppSelector((state) => state.flowchart);
   const isNarrowScreen = useIsNarrowScreen();
   return (
     <DragDropContextWrapper>
-      <SidebarFlowchart />
       <div id="draggable-portal"></div>
       <div
         className={`bg-slate-900 text-white min-h-screen flex flex-col overflow-hidden no-scroll w-full ${
@@ -25,8 +19,6 @@ const FlowchartLayout = ({ children }: { children: React.ReactNode }) => {
       >
         <FlowchartHeader />
         <div className="flex-1">{children}</div>
-        {/* Sticky footer */}
-        {flowchartData && !isNarrowScreen && <FlowchartUnitCounts />}
       </div>
     </DragDropContextWrapper>
   );
