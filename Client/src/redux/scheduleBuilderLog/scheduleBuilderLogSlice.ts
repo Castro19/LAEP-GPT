43c1e69817293efcc08e1a11bc361103b27879cd
Castro19 <ccastroo@calpoly.edu>
2025-05-15 14:00:21 -0700

    small bug for new chats fixed
    previously there was a bug where a new chat was created for every message.

--- Client/src/redux/scheduleBuilderLog/scheduleBuilderLogSlice.ts
+++ Client/src/redux/scheduleBuilderLog/scheduleBuilderLogSlice.ts
@@ -114,8 +114,8 @@ export const sendMessage = createAsyncThunk<
     { getState, dispatch, rejectWithValue }
   ) => {
     const currentLog = getState().scheduleBuilderLog.currentLog;
-    if (!currentLog) return rejectWithValue({ message: "No active log" });
-    let threadId = currentLog.thread_id;
+
+    let threadId = currentLog?.thread_id ?? `temp-${nanoid()}`;
 
     // 1) now open the SSE stream
     try {
@@ -191,9 +191,12 @@ const scheduleBuilderLog = createSlice({
     },
     setScheduleChatId: (st, a: PayloadAction<string>) => {
       st.currentScheduleChatId = a.payload;
+      if (st.currentLog) {
+        st.currentLog.thread_id = a.payload;
+      }
     },
     newScheduleChat: (st) => {
-      st.currentScheduleChatId = nanoid();
+      st.currentScheduleChatId = `temp-${nanoid()}`;
       st.currentLog = {
         thread_id: `temp-${nanoid()}`,
         conversation_turns: [],
