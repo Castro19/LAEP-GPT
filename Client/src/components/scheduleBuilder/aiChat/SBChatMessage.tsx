6f5c179e797feedee13474974c51607cf03aa6ec
Castro19 <ccastroo@calpoly.edu>
2025-05-14 17:02:15 -0700

    Formatting improvements

--- Client/src/components/scheduleBuilder/aiChat/SBChatMessage.tsx
+++ Client/src/components/scheduleBuilder/aiChat/SBChatMessage.tsx
@@ -65,13 +65,11 @@ const SBChatMessage: React.FC<Props> = ({ msg }) => {
   const [open, setOpen] = useState(false);
 
   const variant = isUser
-    ? "bg-gradient-to-r from-indigo-600 to-indigo-700 text-white"
-    : "bg-gradient-to-r from-gray-800 to-gray-700 text-white";
+    ? "bg-gradient-to-r from-indigo-600 to-indigo-700 text-white text-right"
+    : "bg-gradient-to-r from-gray-800 to-gray-700 text-white text-left";
 
   return (
-    <div
-      className={`w-full my-4 flex ${isUser ? "justify-end" : "justify-start"}`}
-    >
+    <div className={`w-fit my-4 ${isUser ? "ml-auto" : "mr-auto"}`}>
       <div className="flex flex-col w-full">
         {/* Main bubble */}
         <div className={`rounded-lg shadow-lg px-3 py-4 ${variant}`}>
