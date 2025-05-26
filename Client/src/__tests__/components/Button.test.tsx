9b5155e3e0cb3480017457e2f288b9fc373b2109
Cristian Castro Oliva <74516049+Castro19@users.noreply.github.com>
2025-05-26 12:38:42 -0700

    Client unit testing setup (#373)
    * setting up client side unit testing
    
    * Update ci.yml

---
+++ Client/src/__tests__/components/Button.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Button } from "@/components/ui/button";
+
+describe("Button Component", () => {
+  it("renders button with correct text", () => {
+    render(<Button>Click me</Button>);
+    expect(
+      screen.getByRole("button", { name: /click me/i })
+    ).toBeInTheDocument();
+  });
+
+  it("calls onClick handler when clicked", () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick}>Click me</Button>);
+
+    fireEvent.click(screen.getByRole("button", { name: /click me/i }));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("can be disabled", () => {
+    render(<Button disabled>Click me</Button>);
+    expect(screen.getByRole("button", { name: /click me/i })).toBeDisabled();
+  });
+});
